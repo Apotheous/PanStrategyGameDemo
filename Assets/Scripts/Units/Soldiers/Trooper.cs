9660cc4a481b8d1ec04959c3ff7860c8ce6a774a
Apotheous <brucoglu@outlook.com>
2024-06-07 01:57:54 +0300

    feat/object-pool: soldier damage system

--- Assets/Scripts/Units/Soldiers/Trooper.cs
+++ Assets/Scripts/Units/Soldiers/Trooper.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trooper : Character
+public class Trooper : Character, IHittable
 {
     public override void SetDamageValue()
     {
         damage = 5;
+    }
+    public void GetHit(int damageValue, GameObject sender)
+    {
+        health -= damageValue;
+        Debug.Log(sender.name);
+        sender.SetActive(false);
+        Death(health, this.gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        int dValue = collision.GetComponent<BulletDamageHolder>().damage_Value;
+        Debug.Log("Soldier on Damage");
+        GetHit(dValue, collision.gameObject);
+    }
+
+    public void Death(int health, GameObject sender)
+    {
+        if (health == 0) { sender.SetActive(false); }
     }
 }
